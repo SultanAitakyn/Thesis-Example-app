3dca0aa78fb6b8ce07cf175aa3301cbce4947d70
Sultan Aitakyn <sultanaitakyn@Sultans-MacBook-Air.local>
2022-06-13 13:06:51 +0600

    base app

---
+++ src/components/ContainerPage/ContainerPage.tsx
@@ -0,0 +1,11 @@
+export function ContainerPage({
+  children,
+}: {
+  children: JSX.Element | JSX.Element[];
+}) {
+  return (
+    <div className="container page">
+      <div className="row"> {children} </div>
+    </div>
+  );
+}
